3ba1ff18bd7c8e6423c4cc50d136a6aaa54d5b76
Nicholas <nasieb@wisc.edu>
2015-12-09 23:02:33 -0600

    GC Authentication testing

--- Code/AppDelegate.swift
+++ Code/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject:AnyObject]?) -> Bool {
         Fabric.with([Crashlytics()])
-        //GCHelper.sharedInstance.authenticateLocalUser()
+        GCHelper.sharedInstance.authenticateLocalUser()
         return true
     }
 
